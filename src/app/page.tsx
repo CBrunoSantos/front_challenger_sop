ef4552f512c21c5e2ecd2d3b4f817cc5a1524953
Carlos Bruno <cbruno.cs12@gmail.com>
2026-02-17 12:28:24 -0300

    feat: estrutura inicial do frontend

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import HomePage from "@/screens/HomePage";
+
+export default function Home() {
+  return (
+    <HomePage/>
+  );
+}
---
+++ src/screens/HomePage.tsx
@@ -0,0 +1,7 @@
+export default function HomePage(){
+    return (
+        <div>
+            <h1>Pagina Principal Home</h1>
+        </div>
+    );
+}
